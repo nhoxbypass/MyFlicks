c60e18a82e4dd541b6d6a8624bd89f5a31c741f3
hieutam <nhoxbypass@gmail.com>
2016-10-16 21:58:46 +0700

    Revert "Revert "update youtube player""
    This reverts commit 04236b0eafcb8bf92ed8adffe01bb16c732da83b.

--- app/src/main/java/com/example/nhoxb/myflicks/activity/MainActivity.java
+++ app/src/main/java/com/example/nhoxb/myflicks/activity/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.nhoxb.myflicks.activity;
 
+import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.nhoxb.myflicks.R;
 import com.example.nhoxb.myflicks.adapter.MovieAdapter;
 import com.example.nhoxb.myflicks.api.MovieApi;
 import com.example.nhoxb.myflicks.model.Movie;
-import com.example.nhoxb.myflicks.model.NowPlaying;
+import com.example.nhoxb.myflicks.model.ListMovie;
+import com.example.nhoxb.myflicks.model.YoutubeTrailer;
+import com.example.nhoxb.myflicks.model.YoutubeVideo;
 import com.example.nhoxb.myflicks.utils.RetrofitUtils;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,10 +30,17 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SELECTED_ITEM_KEY = "selectedItem";
+    public static final String VIDEO_ID_KEY = "videoId";
+
+    int popularPage = 1;
+
     public ListView listView;
     public ProgressBar progressBar;
     private MovieApi mMovieApi;
     private MovieAdapter mMovieAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,24 +48,87 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.list_item);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
 
         mMovieApi = RetrofitUtils.get(getString(R.string.api_key)).create(MovieApi.class);
 
 
-        mMovieApi.getNowPlaying().enqueue(new Callback<NowPlaying>() {
+        mMovieApi.getNowPlaying().enqueue(new Callback<ListMovie>() {
             @Override
-            public void onResponse(Call<NowPlaying> call, Response<NowPlaying> response) {
-                handleResponse(response);
+            public void onResponse(Call<ListMovie> call, Response<ListMovie> response) {
+                handleNowPlayingResponse(response);
             }
 
             @Override
-            public void onFailure(Call<NowPlaying> call, Throwable t) {
+            public void onFailure(Call<ListMovie> call, Throwable t) {
                 Log.e("MOVIE","MOVIE API CALL FAILED " + t.getMessage());
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Get Movie id
+                final Movie movie = mMovieAdapter.getItem(i);
+
+                //Get List trailer with movieId
+                mMovieApi.getTrailers(movie.getId()).enqueue(new Callback<YoutubeTrailer>() {
+                    @Override
+                    public void onResponse(Call<YoutubeTrailer> call, Response<YoutubeTrailer> response) {
+                        List<YoutubeVideo> mTrailers = response.body().getTrailerList();
+
+                        if (!mTrailers.isEmpty())
+                        {
+                            String videoId = mTrailers.get(0).getSource();
+
+                            Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+
+                            Bundle extras = new Bundle();
+                            extras.putSerializable(SELECTED_ITEM_KEY, movie);
+                            intent.putExtra(VIDEO_ID_KEY, videoId);
+                            intent.putExtras(extras);
+
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<YoutubeTrailer> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Get trailers failed",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mMovieApi.getPopular(popularPage).enqueue(new Callback<ListMovie>() {
+                    @Override
+                    public void onResponse(Call<ListMovie> call, Response<ListMovie> response) {
+                        handleNowPlayingResponse(response);
+                        swipeRefreshLayout.setRefreshing(false);
+                        popularPage++;
+                    }
+
+                    @Override
+                    public void onFailure(Call<ListMovie> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+
     }
 
-    private void handleResponse( Response<NowPlaying> response)
+    private void handleNowPlayingResponse( Response<ListMovie> response)
     {
         mMovieAdapter = new MovieAdapter(this,response.body().getMovieList());
         listView.setAdapter(mMovieAdapter);
--- app/src/main/java/com/example/nhoxb/myflicks/api/MovieApi.java
+++ app/src/main/java/com/example/nhoxb/myflicks/api/MovieApi.java
@@ -1,14 +1,25 @@
 package com.example.nhoxb.myflicks.api;
 
-import com.example.nhoxb.myflicks.model.NowPlaying;
+import com.example.nhoxb.myflicks.model.ListMovie;
+import com.example.nhoxb.myflicks.model.YoutubeTrailer;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by nhoxb on 10/12/2016.
  */
 public interface MovieApi {
     @GET("now_playing")
-    Call<NowPlaying> getNowPlaying();
+    Call<ListMovie> getNowPlaying();
+
+    @GET("popular")
+    Call<ListMovie> getPopular(@Query("page") Integer page);
+
+    @GET("{videoId}/trailers")
+    Call<YoutubeTrailer> getTrailers(@Path("videoId") String videoId);
 }
